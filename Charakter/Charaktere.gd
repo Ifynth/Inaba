8e291c01c9c7faa28b4501bf2fc6ce3587aaa707
Ifynth <ifynth@gmail.com>
2018-01-18 18:06:12 +0100

    Adding Charakter

---
+++ Charakter/Charaktere.gd
@@ -0,0 +1,14 @@
+extends Node
+
+func _ready():
+	pass
+	# TODO get a spezific char (wahrscheinlich unnötig)
+	#print(getChar("Xayah"))
+	#print(getChar("Valor"))
+
+
+func getChar(name):
+	# gibt den Char Node zurück per Namen
+	for node in get_children():
+		if node.name == name:
+			return node
---
+++ Charakter/Vorlage/Charakter.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+export (String) var name = ""
+export (int) var leben = 5
+export (int) var angriff = 1
+export (int) var verteidigung = 0
